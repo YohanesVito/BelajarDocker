d0bc05bb2cfed4c1bd01d0c207d1c0bad323d055
YOHANES VITO <yohanes.vito@ti.ukdw.ac.id>
2023-01-30 19:20:33 +0700

    asd

--- Flask/app.py
+++ Flask/app.py
@@ -10,97 +10,43 @@ app.config['MYSQL_DB'] = 'mokura'
 
 mysql = MySQL(app)
    
-@app.route('/data/',methods=['GET'])
-def get_all_datas():
 
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM data")
-    data = cur.fetchall()
-  
-    return jsonify(data)
-    # return jsonify({
-    #     "time_stamp" : data.time_stamp,
-    #     "speed" : data.speed,
-    #     "rpm" : data.rpm,
-    #     "battery": data.battery,
-    #     "lat" : data.lat,
-    #     "lon" : data.lon,
-    #     "compass" : data.compass,
-    #     "duty_cycle" : data.duty_cycle,
-    #     "time_stamp_database" : data.time_stamp.database
-    # })
+@app.route('/login/',methods=['POST'])
+def login_user():
 
-@app.route('/users/',methods=['GET'])
-def get_all_users():
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users")
-    data = cur.fetchall()
-  
-    return jsonify(data)
-
-@app.route('/mokuras/',methods=['GET'])
-def get_all_mokura():
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM mokuras")
-    data = cur.fetchall()
-  
-    return jsonify(data)
-
-# @app.route('/user/<user_id>',methods=['GET'])
-# def get_one_users():
-#     return ''
-
-@app.route('/datalist/',methods=['POST'])
-def insert_datalist():
-    datas = request.get_json()
     
-    # parsing data list
-    for data in datas:
-        time_stamp = data["time_stamp"]
-        lat = data["lat"]
-        lon = data["lon"]
-        compass = data["compass"]
-        speed = data["speed"]
-        rpm = data["rpm"]
-        battery = data["battery"]
-        duty_cycle = data["duty_cycle"]   
+    default_value = "null"
+    email = request.form.get('email', default_value)
+    password = request.form.get('password', default_value)
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO data (time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(time_stamp,speed,rpm,battery,lat,lon,compass,duty_cycle))
-        mysql.connection.commit()
-        cur.close()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT EXISTS (SELECT * FROM users WHERE email = %s AND password = %s)",(email,password))
+    data = cur.fetchall()
 
-    return jsonify({'message': 'data inserted!'})
+    if(data[0][0]==0):
+        fail_response= jsonify({"error": True, "message": "login failed", "loginResult":{"email":email}})
+        return fail_response
+    else:
+        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s",(email,password))
+        user = cur.fetchall()
 
+        return jsonify({"error": False,"message": "login success!", "loginResult":{"idUser":user[0][0],"email":user[0][1],"username":user[0][2],"password":user[0][3]}})
 
-# @app.route('/data/',methods=['POST'])
-# def insert_data():
+@app.route('/register/',methods=['POST'])
+def register_user():
 
-# # kalau datanya bentuk json
-#     # data = request.get_json()
-    
-#     # time_stamp = data["time_stamp"]
-#     # speed = data["speed"]
-#     # rpm = data["rpm"]
-#     # battery = data["battery"]
+    default_value = "null"
+    email = request.form.get('email', default_value)
+    username = request.form.get('username', default_value)
+    password = request.form.get('password', default_value)
 
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (email,username,password) VALUES (%s,%s,%s)",(email,username,password))
+    mysql.connection.commit()
+    cur.close()
 
-# # kalau datanya bentuk x-www-form-urlencoded
-#     default_value = "null"
+    return jsonify({"error": "false", "message": "user registered!"})
 
-#     time_stamp = request.form.get('time_stamp', default_value)
-#     speed = request.form.get('speed', default_value)
-#     rpm = request.form.get('rpm', default_value)
-#     battery = request.form.get('battery', default_value)
-
-#     cur = mysql.connection.cursor()
-#     cur.execute("INSERT INTO data (time_stamp,speed,rpm,battery) VALUES (%s,%s,%s,%s)",(time_stamp,speed,rpm,battery))
-#     mysql.connection.commit()
-#     cur.close()
-
-#     return jsonify({'message': 'data inserted!'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969,debug=True)
